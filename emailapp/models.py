2cd9e61b74a0a8b5b928d32aa10fdb4fd5333979
subedisapana <subedisapana10@gmail.com>
2018-05-27 08:57:00 +0545

    [ADD] A userinfo model and create its migration

--- emailapp/models.py
+++ emailapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class UserInfo(models.Model):
+    email = models.EmailField()
+    password = models.CharField(max_length=50, null=False)
+    host = models.CharField(max_length=50, null=False)
+    port = models.CharField(max_length=5, null=False)
+
